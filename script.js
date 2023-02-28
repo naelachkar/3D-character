d6458b1e056ff35d9ef2ec0a97e8e63b02c4d1d2
Naël Achkar <naelachkar@hotmail.fr>
2023-02-28 11:25:53 +0200

    Following mouse done

--- script.js
+++ script.js
@@ -66,19 +66,40 @@
         let fileAnimations = gltf.animations;
 
         model.traverse((o) => {
+          //* To get the list of all the bones
+          //   if (o.isBone) {
+          //     console.log(o.name);
+          //   }
+
           if (o.isMesh) {
             o.castShadow = true;
             o.receiveShadow = true;
             o.material = stacy_mtl;
           }
+
+          // Reference the neck and waist bones
+          if (o.isBone && o.name === "mixamorigNeck") {
+            neck = o;
+          }
+          if (o.isBone && o.name === "mixamorigSpine") {
+            waist = o;
+          }
         });
 
         // Set the models initial scale
         model.scale.set(7, 7, 7);
-
         model.position.y = -11;
 
         scene.add(model);
+        loaderAnim.remove();
+        mixer = new THREE.AnimationMixer(model);
+
+        let idleAnim = THREE.AnimationClip.findByName(fileAnimations, "idle");
+        idleAnim.tracks.splice(3, 3);
+        idleAnim.tracks.splice(9, 3);
+
+        idle = mixer.clipAction(idleAnim);
+        idle.play();
       },
       undefined, // We don't need this function
       function (error) {
@@ -129,6 +150,9 @@
   }
 
   function update() {
+    if (mixer) {
+      mixer.update(clock.getDelta());
+    }
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -152,5 +176,67 @@
       renderer.setSize(width, height, false);
     }
     return needResize;
+  }
+
+  document.addEventListener("mousemove", function (e) {
+    var mousecoords = getMousePos(e);
+    if (neck && waist) {
+      moveJoint(mousecoords, neck, 50);
+      moveJoint(mousecoords, waist, 30);
+    }
+  });
+
+  function getMousePos(e) {
+    return { x: e.clientX, y: e.clientY };
+  }
+
+  function moveJoint(mouse, joint, degreeLimit) {
+    let degrees = getMouseDegrees(mouse.x, mouse.y, degreeLimit);
+    joint.rotation.y = THREE.Math.degToRad(degrees.x);
+    joint.rotation.x = THREE.Math.degToRad(degrees.y);
+  }
+
+  function getMouseDegrees(x, y, degreeLimit) {
+    let dx = 0,
+      dy = 0,
+      xdiff,
+      xPercentage,
+      ydiff,
+      yPercentage;
+
+    let w = { x: window.innerWidth, y: window.innerHeight };
+
+    // Left (Rotates neck left between 0 and -degreeLimit)
+
+    // 1. If cursor is in the left half of screen
+    if (x <= w.x / 2) {
+      // 2. Get the difference between middle of screen and cursor position
+      xdiff = w.x / 2 - x;
+      // 3. Find the percentage of that difference (percentage toward edge of screen)
+      xPercentage = (xdiff / (w.x / 2)) * 100;
+      // 4. Convert that to a percentage of the maximum rotation we allow for the neck
+      dx = ((degreeLimit * xPercentage) / 100) * -1;
+    }
+    // Right (Rotates neck right between 0 and degreeLimit)
+    if (x >= w.x / 2) {
+      xdiff = x - w.x / 2;
+      xPercentage = (xdiff / (w.x / 2)) * 100;
+      dx = (degreeLimit * xPercentage) / 100;
+    }
+    // Up (Rotates neck up between 0 and -degreeLimit)
+    if (y <= w.y / 2) {
+      ydiff = w.y / 2 - y;
+      yPercentage = (ydiff / (w.y / 2)) * 100;
+      // Note that I cut degreeLimit in half when she looks up
+      dy = ((degreeLimit * 0.5 * yPercentage) / 100) * -1;
+    }
+
+    // Down (Rotates neck down between 0 and degreeLimit)
+    if (y >= w.y / 2) {
+      ydiff = y - w.y / 2;
+      yPercentage = (ydiff / (w.y / 2)) * 100;
+      dy = (degreeLimit * yPercentage) / 100;
+    }
+    return { x: dx, y: dy };
   }
 })(); // Don't add anything below this line
